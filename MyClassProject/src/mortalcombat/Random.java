e9f3aadcf8cb5362efc1af375b52295abd0193c8
KovalevuchMykhaylo <mishamimi@ukr.net>
2016-12-13 18:53:05 +0200

    up

--- MyClassProject/src/mortalcombat/Random.java
+++ MyClassProject/src/mortalcombat/Random.java
@@ -1,7 +1,5 @@
 package mortalcombat;
 
 public class Random {
-	static int random(int min, int max){
-		return (int) Math.round(Math.random()*(max-min)+min);
+	
 	}
-}
--- MyClassProject/src/mortalcombat/Sonya.java
+++ MyClassProject/src/mortalcombat/Sonya.java
@@ -1,6 +1,36 @@
 package mortalcombat;
 
 public class Sonya {
-	int health;
-	int kick;
+	String name = "Sonya";
+	int health = 100;
+	int kick = (int)(Math.random()*11);
+	public Sonya(String name, int health, int kick) {
+		this.name = name;
+		this.health = health;
+		this.kick = kick;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	public int getKick() {
+		return kick;
+	}
+	public void setKick(int kick) {
+		this.kick = kick;
+	}
+	@Override
+	public String toString() {
+		return "Sonya [name=" + name + ", health=" + health + ", kick=" + kick
+				+ "]";
+	}
+	
 }
