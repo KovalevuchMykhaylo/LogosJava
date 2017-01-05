b021f4816034becc95c5917d13ef25c1aa63f986
KovalevuchMykhaylo <mishamimi@ukr.net>
2017-01-05 19:07:28 +0200

    two

--- MyClass/src/home/core9/Main.java
+++ MyClass/src/home/core9/Main.java
@@ -1,6 +1,7 @@
 package home.core9;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Main {
@@ -12,6 +13,47 @@ public class Main {
 				new SteeringWheel(14, true), new Wheel("Winter", 19)));
 		List.add(new Car("BMW", new Body("Alive", "Red"), new SteeringWheel(15,
 				false), new Wheel("Summer", 17)));
+		while (true) {
+			System.out.println("Enter 1 to find body type: ");
+			System.out.println("Enter 2 to delete car by color: ");
+			switch (sc.next()) {
+			case "1":
+				one();
+				break;
+			case "2":
+				two();
+				break;
+
+			default:
+				sc.close();
+				return;
+			}
+		}
+	}
+
+	static void one() {
+		System.out.println("Enter body type: ");
+		String typ = sc.next();
+		Iterator<Car> iter = List.iterator();
+		while (iter.hasNext()) {
+			Car car = iter.next();
+			if (car.getBody().getType().equals(typ))
+				System.out.println(car);
+
+		}
+	}
+
+	static void two() {
+		System.out.println("Enter color: ");
+		String col = sc.next();
+		Iterator<Car> iter = List.iterator();
+		while (iter.hasNext()) {
+			Car car = iter.next();
+			if (car.getBody().getColor().equals(col)) {
+				iter.remove();
+			}
+		}
+		List.forEach(System.out::println);
 	}
 
 }
